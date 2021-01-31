2bc3b82f9f224ab0542a5ab7785585923fd64525
Shubh Gupta <shubhngupta04@gmail.com>
2021-01-31 17:10:45 +0530

    Chatbot code updated

--- Chatbot/app.py
+++ Chatbot/app.py
@@ -1,5 +1,6 @@
 from chatbot import chatbot
 from flask import Flask, render_template, request
+from chat_bot_general_health import ask_question_tfidf
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -11,7 +12,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
+    return ask_question_tfidf(userText)
 
 if __name__ == "__main__":
-    app.run() 
+    app.run()
